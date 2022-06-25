52aefc4432706bdbd5df108c00cd1ff7ab984660
Dmitriy Romanutskyi <71261688+romanutskyi@users.noreply.github.com>
2022-06-25 12:29:26 +0300

    Replace correct script

--- terraform-installation.sh
+++ terraform-installation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget -qO - terraform.gpg https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/terraform-archive-keyring.gpg
-sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/terraform-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/terraform.list
+apt install curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 apt update
 apt install terraform
